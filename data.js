b2ff3ed8fa7097b198747d259d64a4ef1ca6ed88
Arkadiy Seletskiy <arkadius2006@gmail.com>
2017-08-01 05:56:31 +0300

    time support

--- data.js
+++ data.js
@@ -220,7 +220,7 @@ var quotesController = function ($scope, $http) {
         var sum = 0;
         var i, pos;
         var incr;
-        for (i = 0; i <  $scope.position_list.length; i += 1) {
+        for (i = 0; i < $scope.position_list.length; i += 1) {
             pos = $scope.position_list[i];
             incr = toUsdValue(pos['currency'], pos['position']);
             sum += incr;
@@ -238,7 +238,6 @@ var quotesController = function ($scope, $http) {
             // lookup USDxxx or xxxUSD quote
             for (i = 0; i < $scope.quote_list.length; i += 1) {
                 q = $scope.quote_list[i];
-                console.log(JSON.stringify(q));
 
                 symbol = q['symbol'];
                 price = q['price'];
@@ -253,9 +252,107 @@ var quotesController = function ($scope, $http) {
                 }
             }
 
-            console.error('Cannot convert ' + xxx +' to USD');
+            console.error('Cannot convert ' + xxx + ' to USD');
             return 0; // todo handle properly case when we have no USD equivalent for xxx
         }
     };
 
+    // clock todo move to separate js file
+
+    var drawClock = function (ctx, radius) {
+        drawFace(ctx, radius);
+        drawNumbers(ctx, radius);
+        drawTime(ctx, radius);
+    };
+
+    var drawFace = function (ctx, radius) {
+        var grad;
+
+        ctx.beginPath();
+        ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+        ctx.fillStyle = 'white';
+        ctx.fill();
+
+        grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+        grad.addColorStop(0, '#333');
+        grad.addColorStop(0.5, 'white');
+        grad.addColorStop(1, '#333');
+        ctx.strokeStyle = grad;
+        ctx.lineWidth = radius * 0.1;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+        ctx.fillStyle = '#333';
+        ctx.fill();
+    };
+
+    function drawNumbers(ctx, radius) {
+        var ang;
+        var num;
+        ctx.font = radius * 0.15 + "px arial";
+        ctx.textBaseline = "middle";
+        ctx.textAlign = "center";
+        for (num = 1; num < 13; num++) {
+            ang = num * Math.PI / 6;
+            ctx.rotate(ang);
+            ctx.translate(0, -radius * 0.85);
+            ctx.rotate(-ang);
+            ctx.fillText(num.toString(), 0, 0);
+            ctx.rotate(ang);
+            ctx.translate(0, radius * 0.85);
+            ctx.rotate(-ang);
+        }
+    }
+
+    var drawTime = function (ctx, radius) {
+        var now = new Date();
+        var hour = now.getHours();
+        var minute = now.getMinutes();
+        var second = now.getSeconds();
+        //hour
+        hour = hour % 12;
+        hour = (hour * Math.PI / 6) + (minute * Math.PI / (6 * 60)) + (second * Math.PI / (360 * 60));
+        drawHand(ctx, hour, radius * 0.5, radius * 0.07);
+        //minute
+        minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
+        drawHand(ctx, minute, radius * 0.8, radius * 0.07);
+        // second
+        second = (second * Math.PI / 30);
+        drawHand(ctx, second, radius * 0.9, radius * 0.02);
+    };
+
+    var drawHand = function (ctx, pos, length, width) {
+        ctx.beginPath();
+        ctx.lineWidth = width;
+        ctx.lineCap = "round";
+        ctx.moveTo(0, 0);
+        ctx.rotate(pos);
+        ctx.lineTo(0, -length);
+        ctx.stroke();
+        ctx.rotate(-pos);
+    };
+
+    var getClockParam = function (city) {
+        console.log('getparams lodon');
+        var citycanvas = document.getElementById(city);
+        var cityctx = citycanvas.getContext("2d");
+        var cityraduis = citycanvas.height / 2;
+        cityctx.translate(cityraduis, cityraduis);
+        return {'ctx': cityctx, 'radius': cityraduis};
+    };
+
+    var startClock = function (city) {
+        var params = getClockParam(city);
+
+        setInterval(function () {
+            drawClock(params['ctx'], params['radius'])
+        }, 1000);
+    };
+
+    // todo fix time zones
+    startClock('london');
+    startClock('tokio');
+    startClock('newyork');
+    startClock('moscow');
 };
